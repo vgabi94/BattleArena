94963346f9aea7149039c0d5eadf93a536b78e4e
Gabriel Vaduva <vgabriel@engineer.com>
2017-10-12 23:47:54 +0300

    Added new assets and implemented Health component. Added test scene. Added a singleton for managing world events more easily.

---
+++ Assets/Scripts/EventObserver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObservableEvents
+{
+    
+}
+
+public class EventObserver
+{
+    private EventObserver() { }
+
+    private static EventObserver instance;
+
+    public static EventObserver Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new EventObserver();
+            return instance;
+        } 
+    }
+
+    public delegate void SenderTarget(GameObject sender, GameObject target);
+
+    public void Notify(ObservableEvents ev, params object[] args)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxHP = 100f;
+    public float minHP = 0f;
+
+    private float hpValue;
+
+    public float HP
+    {
+        get { return hpValue; }
+        set
+        {
+            hpValue = Mathf.Clamp(value, minHP, maxHP);
+            BroadcastMessage("HPChanged", hpValue, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    void ApplyDamage(float damage, GameObject sender)
+    {
+        HP -= damage;
+        BroadcastMessage("HitBy", sender, SendMessageOptions.DontRequireReceiver);
+    }
+}
